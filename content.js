2f47a63279ee1b4c9ef5b9e68fae8b2d4ba61185
jbr1gh7 <54848093+jbr1gh7@users.noreply.github.com>
2020-05-13 20:19:06 +0100

    selection of text and put into string finished
    also added crude character count, html injected into site but will try to use shadow dom instead

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 let selectionList = [];
 let finalSelection = "";
-let test = [];
+let validTags = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "SPAN", "LI", "A", "STRONG", "B", "CITE", "DFN", "EM", "I", "KBD", "LABEL", "Q", "SMALL", "BIG", "SUB", "SUP", "TIME", "VAR"];
 
 //messaging callback to send data between js files
 function handleContentRequests(message, sender, sendResponse) {
@@ -39,7 +39,6 @@ function searchForContext(targetNode, selection) {
     }
 
     let indexFound = newSearch(fullText, selection);
-    console.log("indexFound: ", indexFound);
     return [fullText, indexFound];
 
 }
@@ -49,7 +48,6 @@ function getParentNode(targetNode) {
     let newWholeText = null;
 
     let getNextParent = function(child) {
-        let validTags = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "SPAN", "LI", "A", "STRONG", "B", "CITE", "DFN", "EM", "I", "KBD", "LABEL", "Q", "SMALL", "BIG", "SUB", "SUP", "TIME", "VAR"];
         let finished = false;
 
         for (let i=0; i<validTags.length; i++) {
@@ -83,45 +81,41 @@ function getParentNode(targetNode) {
 }
 
 /*
-    let newWholeText = "";
-    //outer means that the wholeText is cut off by a 'sibling' inline tag, so the recursive function adds text value of each nextSibling node until there are none left, assigning newWholeText to this value
-        let siblingArray = [];
-        siblingArray.push(targetNode.wholeText);
-        if (targetNode.nextSibling.nodeName == "#text") {
-            siblingArray.push(targetNode.nextSibling.wholeText);
-        }
-        else {
-            siblingArray.push(targetNode.nextSibling.innerText);
-        }
+function getSiblingNodes(targetNode) {
+    let siblingArray = [];
+    siblingArray.push(targetNode.wholeText);
+    if (targetNode.nextSibling.nodeName == "#text") {
+        siblingArray.push(targetNode.nextSibling.wholeText);
+    }
+    else {
+        siblingArray.push(targetNode.nextSibling.innerText);
+    }
         
-        let pushSibling = function(sibling) {
-            console.log("siblingArray", siblingArray);
-            console.log("sibling.nextSibling", sibling.nextSibling);
-            if (sibling.nextSibling != null) {
-                if (sibling.nextSibling.nodeName == "#text") {
-                    siblingArray.push(sibling.nextSibling.wholeText);
-                }
-                else {
-                    siblingArray.push(sibling.nextSibling.innerText);
-                }
-                pushSibling(sibling.nextSibling)
-                console.log("not finished");
+    let pushSibling = function(sibling) {
+        console.log("siblingArray", siblingArray);
+        console.log("sibling.nextSibling", sibling.nextSibling);
+        if (sibling.nextSibling != null) {
+            if (sibling.nextSibling.nodeName == "#text") {
+                siblingArray.push(sibling.nextSibling.wholeText);
             }
             else {
-                console.log("finished");
-                return
+                siblingArray.push(sibling.nextSibling.innerText);
             }
-        };
+            pushSibling(sibling.nextSibling)
+                console.log("not finished");
+        }
+        else {
+            console.log("finished");
+            return
+        }
+    };
         
-        pushSibling(targetNode.nextSibling);
-        newWholeText = siblingArray.join("");
-    
-    //inner means that the wholeText is part of an inline tag, so the recursive function moves up the family tree until it reaches the last appropriate parent of the inital node, assigning newWholeText to this value
-    
-        
+    pushSibling(targetNode.nextSibling);
+    newWholeText = siblingArray.join("");     
 }
 */
-//goes is function above
+//^^^reursive function for getting sibling nodes
+
 
 
 //autocompletes first selected word.
@@ -156,12 +150,12 @@ function completeLastWord(targetNode, selection) {
 }
 
 function filterSelectedNodes(liveList) {
-    let validTags = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "LI"];
     let staticArray = [];
+    let blockTextTags = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "LI"];
 
     for (let i=0; i<liveList.length; i++) {
-        for (let j=0; j<validTags.length; j++) {
-            if (liveList[i].tagName == validTags[j]) {staticArray.push(liveList[i])}
+        for (let j=0; j<blockTextTags.length; j++) {
+            if (liveList[i].tagName == blockTextTags[j]) {staticArray.push(liveList[i])}
         }
     }
 
@@ -176,66 +170,87 @@ function pushFilteredNodes(staticArray) {
     }
 }
 
+function testRange(range) {
+    let finished = false;
+    for (let i=0; i<validTags.length; i++) {
+        if (range.commonAncestorContainer.nodeName == validTags[i]) {
+            finished = true;
+        }
+
+        if (finished == true) {
+            return true;
+        }
+        else if (finished == false && i == validTags.length) {
+            return false;
+        }
+    }
+}
+
 /*
 function begunSelecting() {
     var iframe = document.createElement('iframe');
     iframe.classList.add("char-count-iframe");
     iframe.src = chrome.extension.getURL('/character_count.html');
     document.body.appendChild(iframe);
-
     console.log("iframe: ", iframe);
     let iframeWindow = iframe.contentWindow;
-
     console.log("iframeWindow", iframeWindow);
     console.log("iframe.src", iframe.src);
+    let checkCharCount = function() {
+        let initSelection = window.getSelection();
+        let charCount = initSelection.toString().length;
+        return charCount;
+    }
+    setInterval(function(){
+        console.log("iframeWindow in setinterval function", iframeWindow);
+        //iframeWindow.getElementById("#char-count-value").textContent = checkCharCount();
+        //console.log("charCount: ", checkCharCount())
+    }, 1000);
+}
+*/
+//iframe method ^^^
 
+function updateCharCount() {
     let checkCharCount = function() {
         let initSelection = window.getSelection();
         let charCount = initSelection.toString().length;
         return charCount;
     }
 
-    setInterval(function(){
-        console.log("iframeWindow in setinterval function", iframeWindow);
-        //iframeWindow.getElementById("#char-count-value").textContent = checkCharCount();
-        //console.log("charCount: ", checkCharCount())
-
-    }, 1000);
+    $("#char-count-value").text(checkCharCount());
+    $("#char-count-container").css({position: "absolute", left: event.pageX + 75, top: event.pageY + 75});
+    console.log("char count: ", checkCharCount());
 }
-*/
-//iframe method ^^^
+//this updates whenever mouse moves, only need to do it while mouse is held, maybe addeventlistener inside begunselecting function?
 
 function begunSelecting() {
     $.get(chrome.extension.getURL('/character_count.html'), function(data) {
         $(data).appendTo('body')
     });
 
-    let checkCharCount = function() {
-        let initSelection = window.getSelection();
-        let charCount = initSelection.toString().length;
-        return charCount;
-    }
-
-    $(document).on("mousemove", function(event) {
-        $("#char-count-value").text(checkCharCount());
-        $("#char-count-container").css({position: "absolute", left: event.pageX + 75, top: event.pageY + 75});
-    });
+    window.addEventListener("mousemove", updateCharCount);
 }
+
 
 //selection callback function
 function doneSelecting() {
+    window.removeEventListener("mousemove", updateCharCount);
+    window.removeEventListener("mousedown", begunSelecting);
     let selectionObj = window.getSelection();
     let selection = selectionObj.toString();
     console.log("selectionObj: ", selectionObj);
     
-    if (selection.length > 0) {
-        if (selectionObj.anchorNode == selectionObj.focusNode) {
-            console.log(completeFirstWord(selectionObj.anchorNode, completeLastWord(selectionObj.anchorNode, selection)));
+    if (!selectionObj.isCollapsed) {
+        let range = selectionObj.getRangeAt(0);
+        console.log("range: ", range);
+
+        if (selectionObj.anchorNode == selectionObj.focusNode || testRange(range) == true) {
+            console.log("outcome: ", completeFirstWord(selectionObj.anchorNode, completeLastWord(selectionObj.anchorNode, selection)));
         }
-        else {
-            let range = selectionObj.getRangeAt(0);
+        else if (selectionObj.anchorNode != selectionObj.focusNode) {
             let liveNodeList = range.cloneContents().querySelectorAll('*');
             let staticNodeArray = filterSelectedNodes(liveNodeList);
+            console.log("staticNodeArray: ", staticNodeArray);
 
             //if selection went up the page from starting point or down the page from starting point
             if (selectionObj.anchorNode.wholeText.search(staticNodeArray[staticNodeArray.length-1].innerText) == -1) {
@@ -247,38 +262,28 @@ function doneSelecting() {
                 selectionList.push(completeFirstWord(selectionObj.focusNode, staticNodeArray[0].innerText));
                 pushFilteredNodes(staticNodeArray);
                 selectionList.push(completeLastWord(selectionObj.anchorNode, staticNodeArray[staticNodeArray.length-1].innerText));
-            }
-
+            }  
+            
             //concatenating text values of filtered selected elements
             finalSelection = selectionList.join(" ");
             
             console.log("selectionList: ", selectionList);
             //console.log("staticNodeArray: ", staticNodeArray);
-            console.log("selectionObj: ", selectionObj);
+            //console.log("selectionObj: ", selectionObj);
             console.log("======================OUTCOME======================");
             console.log(finalSelection);
             console.log("***************************************************");
-            
-        }
-        selectionList = [];
-        finalSelection = "";
+        }            
     }
+    selectionList = [];
+    finalSelection = "";
+    window.addEventListener("mousedown", begunSelecting);
+    window.addEventListener("mouseup", doneSelecting);
 }
+
 
 //event listeners
 chrome.runtime.onMessage.addListener(handleContentRequests);
 window.addEventListener("mousedown", begunSelecting);
 window.addEventListener("mouseup", doneSelecting);
 
-
-/*
-fetch(chrome.extension.getURL('/input-box.html'))
-    .then(response => response.text())
-    .then(data => {
-        document.body.innerHTML += data;
-        // other code
-        // add event listeners or logic to connect to other parts
-    }).catch(err => {
-        // handle error
-});
-*/

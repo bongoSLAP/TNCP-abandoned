1b9b19e2b59d487dd6b403d74c5aee13604af8f8
jbr1gh7 <54848093+jbr1gh7@users.noreply.github.com>
2020-07-06 08:32:22 +0100

    added radio buttons to begin dev of user input

--- content.js
+++ content.js
@@ -1,12 +1,17 @@
 let selectionList = [];
 let autoCompOutcome = "";
 let validTags = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "SPAN", "LI", "A", "STRONG", "B", "CITE", "DFN", "EM", "I", "KBD", "LABEL", "Q", "SMALL", "BIG", "SUB", "SUP", "TIME", "VAR"];
+
 let checkSelectMade = undefined;
 let updateCharCount = undefined;
 let contextMenuContainer = undefined;
 let charCountText = undefined;
 let selectionMenu = undefined;
+let selectionMade = undefined;
+let exitButton = undefined;
+
 let isClicked = false;
+let isSelectMade = false;
 let isOverLimit = false;
 let isExpanded = false;
 
@@ -26,7 +31,7 @@ function handleContentRequests(message, sender, sendResponse) {
 }
 
 function validateRegExp(string) {
-    //filtering out line breaks
+    //filtering out line breaks etc
     string = string.replace(/(\r\n|\n|\r)/gm, "");
     //special chars can be selected and the search will still work
     string = string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -202,44 +207,66 @@ function autoCompSelection() {
 
 function styleShadowDom(root, selector, properties) {
     let newDeclaration = undefined;
-    if (typeof selector === "number") {
-        if (selector > 0 && selector < root.styleSheets[0].cssRules.length-1) {newDeclaration = root.styleSheets[0].cssRules[selector].style}
-        else {
-            console.log("ERROR: the index '" + selector + "' is out of range.")
-            return;
-        }
-    }
-    else if (typeof selector === "string") {
-        for (let i=0; i<root.styleSheets[0].cssRules.length; i++) {
-            if (selector == root.styleSheets[0].cssRules[i].selectorText) {
-                newDeclaration = root.styleSheets[0].cssRules[i].style
-                break;
-            }
-            
-            if (i == root.styleSheets[0].cssRules.length-1) {
-                console.log("ERROR: the selector '" + selector + "' does not exist.");
+
+    let checkDataType = function(thisSelector) {
+        if (typeof thisSelector === "number") {
+            if (thisSelector > 0 && thisSelector < root.styleSheets[0].cssRules.length-1) {newDeclaration = root.styleSheets[0].cssRules[thisSelector].style}
+            else {
+                console.log("ERROR: the index '" + thisSelector + "' is out of range.")
                 return;
             }
         }
-    }
-    else {
-        console.log("ERROR: the datatype of selector '" + selector + "' is invalid.")
-        return;
+        else if (typeof thisSelector === "string") {
+            for (let i=0; i<root.styleSheets[0].cssRules.length; i++) {
+                if (thisSelector == root.styleSheets[0].cssRules[i].selectorText) {
+                    newDeclaration = root.styleSheets[0].cssRules[i].style
+                    break;
+                }
+                
+                if (i == root.styleSheets[0].cssRules.length-1) {
+                    console.log("ERROR: the selector '" + thisSelector + "' does not exist.");
+                    return;
+                }
+            }
+        }
+        else {
+            console.log("ERROR: the datatype of selector '" + thisSelector + "' is invalid.")
+            return;
+        }
     }
 
-    if (properties.length == 0) {console.log("ERROR: empty property array given")}
-    else if (properties.length == 1) {
-        newDeclaration.setProperty(properties[0][0], properties[0][1])
+    let applyChanges = function() {
+        if (properties.length == 0) {console.log("ERROR: empty property array given")}
+        else if (properties.length == 1) {
+            newDeclaration.setProperty(properties[0][0], properties[0][1])
+        }
+        else {
+            for (let j=0; j<properties.length; j++) {
+                newDeclaration.setProperty(properties[j][0], properties[j][1]);
+            }
+        }
+    }
+
+    if (Array.isArray(selector)) {
+        for (let j=0; j<selector.length; j++) {
+            checkDataType(selector[j]);
+            applyChanges();
+        }
     }
     else {
-        for (let j=0; j<properties.length; j++) {
-            newDeclaration.setProperty(properties[j][0], properties[j][1]);
-        }
+        checkDataType(selector);
+        applyChanges();
     }
 }
 
 function whenNotHovering(element, callback) {
     if (!element.matches(':hover')) {
+        callback();
+    }
+}
+
+function whenHovering(element, callback) {
+    if (element.matches(':hover')) {
         callback();
     }
 }
@@ -251,8 +278,12 @@ function setToMousePos(event) {
     ]);
 }
 
+function exitContextMenu() {
+    console.log("exit button clicked")
+    contextMenuContainer.classList.add("hidden");
+}
+
 function begunSelecting() {
-    let isSelectMade = false;
     if (!isClicked) {
         let fontRule = document.createElement("style");
         fontRule.innerText = `
@@ -276,8 +307,37 @@ function begunSelecting() {
         container.className = "hidden";
         container.innerHTML = `
             <img id="exit-button" class="hidden" src="` + chrome.runtime.getURL("images/exit-button.png") + `" alt="exit" height="15" width="15">
-            <p class="char-count char-count-text"><span id="char-count-value" class="char-count char-count-text"></span>/200</p>
-            <p id="selection-quotes" class="selection-menu selection-menu-text hidden quotes-font">‘<span id="selection-made" class="selection-menu selection-menu-text"></span>’</p>`
+            <div class="char-count">
+                <p class="char-count-text"><span id="char-count-value" class="char-count-text"></span>/100</p>
+            </div>
+            <div id="selection-menu" class="hidden">
+                <div class="selection-menu-output">
+                    <p id="selection-quotes" class="selection-menu-text hidden quotes-font">‘<span id="selection-made" class="selection-menu-text"></span>’</p>
+                </div>
+
+                <br>
+
+                <div id="argument-nature-container">Nature of argument
+                    <br>
+                    <input class="selection-menu-radios" type="radio" id="for-radio" name="argument-nature" value="for">
+                    <label for="for">For</label><br>
+                    <input class="selection-menu-radios" type="radio" id="against-radio" name="argument-nature" value="against">
+                    <label for="against">Against</label><br>
+                    <input class="selection-menu-radios" type="radio" id="other-radio" name="argument-nature" value="other">
+                    <label for="other">Other</label><br>
+                </div>
+
+                <br>
+                
+                <div id="source-container">Have a source?
+                    <br>
+                    <input class="selection-menu-radios" type="radio" id="yes-source-radio" name="source" value="yes">
+                    <label for="yes">Yes</label><br>
+                    <input class="selection-menu-radios" type="radio" id="no-source-radio" name="source" value="no">
+                    <label for="no">No</label><br>
+                    <button id="confirm-choices">Confirm</button>
+                </div>
+            </div>`
         ;
 
         let shadowDomStyles = document.createElement("style");
@@ -289,7 +349,6 @@ function begunSelecting() {
                 background-color: rgba(230, 230, 230, 0.8);
                 padding: 0px;
                 border-radius: 2.5px 10px 10px 10px;
-                text-align: center;
                 z-index: 100
             }
 
@@ -299,9 +358,16 @@ function begunSelecting() {
                 margin-right: 5px
             }
 
+            #exit-button:hover {
+                cursor: pointer
+            }
+
+            .selection-menu-output {
+                text-align: center
+            }
+
             #selection-quotes {
                 margin-top: 4px;
-                margin-left: 7px;
                 font-weight: bold;
                 font-size: 32px;
                 font-family: 'Revalia', cursive;
@@ -311,6 +377,29 @@ function begunSelecting() {
                 font-weight: normal;
                 font-size: 14px;
                 font-family: Arial
+            }
+
+            .selection-menu-radios {
+                margin-left: 40px
+            }
+
+            #argument-nature-container {
+                text-align: left;
+                margin-left: 20px
+            }
+
+            #source-container {
+                text-align: left;
+                margin-left: 20px
+            }
+
+            #confirm-choices {
+                margin-left: 10px;
+                margin-top: 10px;
+            }
+
+            .char-count {
+                text-align: center
             }
 
             .char-count-text {
@@ -349,7 +438,7 @@ function begunSelecting() {
                     width: 100px;
                 }
                 100% {
-                    height: 120px;
+                    height: 240px;
                     width: 480px;
                 }
             }
@@ -387,62 +476,72 @@ function begunSelecting() {
 
         contextMenuContainer = shadowRoot.querySelector("#context-menu-container");
         charCountText = shadowRoot.querySelector(".char-count-text");
-        selectionMenu = shadowRoot.querySelector(".selection-menu");
-
+        selectionMenu = shadowRoot.querySelector("#selection-menu");
+        selectionMade = shadowRoot.querySelector("#selection-made");
+        exitButton = shadowRoot.querySelector("#exit-button");
+        exitButton.addEventListener("click", exitContextMenu);
+        
         console.log("shadowRoot: ", shadowRoot);
         console.log("document: ", document);
         console.log("container: ", container);    
         console.log("classList: ", contextMenuContainer.classList.value);
     }
     else {
-        if (isExpanded) {
-            selectionMenu.classList.add("hidden");
-            shadowRoot.querySelector("#exit-button").classList.add("hidden");
-            contextMenuContainer.classList.remove("expand-anim");
-            charCountText.classList.add("fadein-anim");
-
-            setTimeout(function() {
-                charCountText.classList.remove("hidden");
-                charCountText.classList.remove("fadein-anim");
-            }, 150);
-
-            isExpanded = false;
-
-            console.log("YES");
-        }
+        whenNotHovering(contextMenuContainer, function() {
+            if (isExpanded) {
+                console.log("clicked out of container bounds")
+                styleShadowDom(shadowRoot, ["#selection-quotes", "#exit-button", "#argument-nature-container", "#source-container"], [["display", "none"]]);
+                contextMenuContainer.classList.remove("expand-anim");
+                charCountText.classList.add("fadein-anim");
+    
+                setTimeout(function() {
+                    charCountText.classList.remove("hidden");
+                    charCountText.classList.remove("fadein-anim");
+                }, 150);
+            }
+        })
     }
 
     checkSelectMade = setInterval(function() {
-        let initSelection = window.getSelection().toString();
+        whenNotHovering(contextMenuContainer, function() {
+            let initSelection = window.getSelection().toString();
 
-        if (initSelection.length > 0) {
-            isSelectMade = true;
-            contextMenuContainer.classList.remove("hidden");
-        }
+            if (initSelection.length > 0) {
+                isSelectMade = true;
+                contextMenuContainer.classList.remove("hidden");
+                console.log("over 0", isSelectMade);
+            }
+            else {
+                isSelectMade = false
+                console.log("not over 0", isSelectMade);
+
+            }
+        })
     }, 50);
 
     updateCharCount = setInterval(function() {
-        if (isSelectMade) {
-            let countLimit = 200; 
-            let rgb = "";
-            selectionList = [];
+            if (isSelectMade) {
+                let countLimit = 100; 
+                let rgb = "";
+                selectionList = [];
 
-            let currentSelection = autoCompSelection();
-            charCount = currentSelection.length;
-            shadowRoot.querySelector("#char-count-value.char-count").innerText = charCount;
+                let currentSelection = autoCompSelection();
+                charCount = currentSelection.length;
+                
+                shadowRoot.querySelector("#char-count-value").innerText = charCount;
 
-            if (charCount > countLimit) {
-                rgb = "rgba(255, 96, 96, 0.8)"
-                isOverLimit = true;
+                if (charCount > countLimit) {
+                    rgb = "rgba(255, 96, 96, 0.8)"
+                    isOverLimit = true;
+                }
+                else {
+                    rgb = "rgba(230, 230, 230, 0.8)"
+                    isOverLimit = false;
+                }
+
+                styleShadowDom(shadowRoot, "#context-menu-container", [["background-color", rgb]]);
+                whenNotHovering(contextMenuContainer, function() {window.addEventListener("mousemove", setToMousePos)});
             }
-            else {
-                rgb = "rgba(230, 230, 230, 0.8)"
-                isOverLimit = false;
-            }
-
-            styleShadowDom(shadowRoot, "#context-menu-container", [["background-color", rgb]]);
-            window.addEventListener("mousemove", setToMousePos);
-        }
     }, 100);
 
     whenNotHovering(contextMenuContainer, function() {
@@ -453,6 +552,7 @@ function begunSelecting() {
     })
         
     isClicked = true;
+    console.log("isSelectMade: ", isSelectMade);
 }
 
 //selection callback function
@@ -477,41 +577,48 @@ function doneSelecting() {
         }, 350)
     }
     else {
-        let finalSelection = autoCompSelection();
-        styleShadowDom(shadowRoot, "#context-menu-container", [["background-color", "rgb(230, 230, 230)"]]);
-        charCountText.classList.add("fadeout-anim");
-        contextMenuContainer.classList.add("expand-anim");
-
-        setTimeout(function() {
-            charCountText.classList.add("hidden");
-            charCountText.classList.remove("fadeout-anim");
-            selectionMenu.classList.add("fadein-anim");
-            shadowRoot.querySelector("#exit-button").classList.add("fadein-anim");
-            isExpanded = true;
+        if (isSelectMade) {
+            if (event.detail === 3) {contextMenuContainer.classList.add("hidden")}
+            let finalSelection = autoCompSelection();
+            styleShadowDom(shadowRoot, "#context-menu-container", [["background-color", "rgb(230, 230, 230)"]]);
+            charCountText.classList.add("fadeout-anim");
+            contextMenuContainer.classList.add("expand-anim");
 
             setTimeout(function() {
-                if (finalSelection.length > 50) {shadowRoot.querySelector("#selection-made.selection-menu").innerText = finalSelection.substr(0, 50) + "...";}
-                else {shadowRoot.querySelector("#selection-made.selection-menu").innerText = finalSelection}
-                selectionMenu.classList.remove("hidden");
-                shadowRoot.querySelector("#exit-button").classList.remove("hidden");
+                charCountText.classList.add("hidden");
+                charCountText.classList.remove("fadeout-anim");
+                selectionMenu.classList.add("fadein-anim");
+                exitButton.classList.add("fadein-anim");
+                isExpanded = true;
 
-                shadowRoot.querySelector("#exit-button").onclick = function() {
-                    contextMenuContainer.classList.add("hidden");
-                }
+                setTimeout(function() {
+                    whenNotHovering(contextMenuContainer, function() {
+                        if (finalSelection.length > 50) {selectionMade.innerText = finalSelection.substr(0, 50) + "...";}
+                        else {selectionMade.innerText = finalSelection}
+                    });
+                    selectionMenu.classList.remove("hidden");
+                    styleShadowDom(shadowRoot, ["#selection-quotes", "#exit-button", "#argument-nature-container", "#source-container"], [["display", "inline"]]);
+                }, 150)
             }, 150)
-        }, 150)
+        }
+        else {
+            console.log("no select made: ", isSelectMade);
+            contextMenuContainer.classList.add("hidden")
+        }
     }
 
     selectionList = [];
     autoCompOutcome = "";
+    //isSelectMade = false;
     
     window.addEventListener("mousedown", begunSelecting);
     window.addEventListener("mouseup", doneSelecting);
-    console.log("contextMenuContainer.classList: ", contextMenuContainer.classList);
+    //console.log("contextMenuContainer.classList: ", contextMenuContainer.classList);
 }
 
 //event listeners
 chrome.runtime.onMessage.addListener(handleContentRequests);
 window.addEventListener("mousedown", begunSelecting);
 window.addEventListener("mouseup", doneSelecting);
+
 
